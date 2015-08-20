6dab09e54a5ccca089c50daf8ebfafb591a5e25d
Ryan Bowler <rbowler0707@gmail.com>
2015-08-19 20:24:28 -0700

    Added some posts/gets

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,19 @@
 get '/' do
   erb :index
 end
+
+get '/login' do
+  erb :login
+end
+
+get '/profile' do
+  erb :profile
+end  
+
+post '/login' do
+  redirect '/'
+end
+
+post '/profile' do
+  redirect '/'  
+end
